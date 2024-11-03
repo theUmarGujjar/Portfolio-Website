b5e046a12657a544960b97f3eaff79ccd13803f7
Umar Gujjar <umar4128015@gmail.com>
2024-11-04 03:54:31 +0500

    Final Form

--- app.js
+++ app.js
@@ -1,4 +1,36 @@
-var typed = new Typed('#element', {
-    strings: ['HTML', 'CSS'  , 'BootStrap' , 'Github' ,'JS'],
-    typeSpeed: 100,
+
+ 
+let menuList = document.querySelectorAll(".menus");
+let menu_btn = document.querySelectorAll(".menu_btn");
+
+menu_btn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    menuList.forEach((item) => {
+      item.classList.toggle("menuDisplay");
+    });
   });
+});
+
+// menu_btn.forEach((btn) => {
+//   document.addEventListener("click", () => {
+//     menuList.forEach((item) => {
+//       item.classList.remove("menuDisplay");
+//     });
+//   });
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
